0fe144bf658338f893b582d2d289200aaa11536d
LucentFlux <>
2023-10-26 18:05:04 +0000

    Update from https://github.com/LucentFlux/voxgame/commit/fbecfd3bbc0f7fae485e7b3782b092c074957b5d

---
+++ nightly/demo/demo.d.ts
@@ -0,0 +1,56 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function dummy_main(): void;
+/**
+*/
+export function run(): void;
+/**
+* Entry point for web workers
+* @param {number} ptr
+*/
+export function wasm_thread_entry_point(ptr: number): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly main: (a: number, b: number) => number;
+  readonly dummy_main: () => void;
+  readonly run: () => void;
+  readonly wasm_thread_entry_point: (a: number) => void;
+  readonly memory: WebAssembly.Memory;
+  readonly __wbindgen_export_1: (a: number, b: number) => number;
+  readonly __wbindgen_export_2: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_export_3: WebAssembly.Table;
+  readonly __wbindgen_export_4: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_5: (a: number, b: number) => void;
+  readonly __wbindgen_export_6: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_7: (a: number) => void;
+  readonly __wbindgen_export_8: (a: number, b: number, c: number, d: number) => void;
+  readonly __wbindgen_thread_destroy: (a: number, b: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+* @param {WebAssembly.Memory} maybe_memory
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput, maybe_memory?: WebAssembly.Memory): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+* @param {WebAssembly.Memory} maybe_memory
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>, maybe_memory?: WebAssembly.Memory): Promise<InitOutput>;
---
+++ nightly/demo/demo_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export function main(a: number, b: number): number;
+export function dummy_main(): void;
+export function run(): void;
+export function wasm_thread_entry_point(a: number): void;
+export const memory: WebAssembly.Memory;
+export function __wbindgen_export_1(a: number, b: number): number;
+export function __wbindgen_export_2(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export function __wbindgen_export_4(a: number, b: number, c: number): void;
+export function __wbindgen_export_5(a: number, b: number): void;
+export function __wbindgen_export_6(a: number, b: number, c: number): void;
+export function __wbindgen_export_7(a: number): void;
+export function __wbindgen_export_8(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_thread_destroy(a: number, b: number): void;
+export function __wbindgen_start(): void;
